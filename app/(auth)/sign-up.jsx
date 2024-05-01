ea7f174beb58e7770c913c40a3839e471f68a134
hizki-rp <hizkielas@gmail.com>
2024-05-01 13:00:15 +0300

    first commit

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const signUp = () => {
+  return (
+    <View>
+      <Text>sign-up</Text>
+    </View>
+  )
+}
+
+export default signUp
+
+const styles = StyleSheet.create({})
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        poppins_bold: ["Poppins-Bold"],
+        poppins_black: ["Poppins-Black"],
+        poppins_extraBold: ["Poppins-ExtraBold"],
+        poppins_extraLight: ["Poppins-ExtraLight"],
+        poppins_light: ["Poppins-Light"],
+        nunito: ["Nunito"],
+        arial: ["Arial"],
+      },
+      
+      colors: {
+        primary: "#04e5d2",
+        secondary: "#EEF500",
+        tertiary: "#0be754",
+        quaternary: "#0f0f30",
+        quinary: "#4e4eef",
+        senary: "#065ee2",
+        senary: "#FBFBFB",
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
